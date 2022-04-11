796c7095bb48d05392755b18713290536002413b
Mofeng <363034937@qq.com>
2022-04-11 23:27:20 +0800

    consts_sample

---
+++ host_agents/CONSTS_SAMPLE.py
@@ -0,0 +1,20 @@
+# DB NAME
+DB_USER = 'userDataBase.json'
+DB_AGENT = 'agentDataBase.json'
+
+# influxDB
+INFLUXDB_URL = "http://influxdb.me.com"
+
+INFLUXDB_PUBLIC_ORG = 'PUBLIC_ORG'
+INFLUXDB_PUBLIC_ORG_ID = '4basdfsdnf65b853'
+INFLUXDB_PUBLIC_ORG_ADMIN_TOKEN = '6yy19Rvuhrnv6Bu6yy19Rvuhrn1sJjV-bofyy19Rvuhrnv6Bu6yy19f2g5NIF6Dg=='
+
+# Grafana
+
+GRAFANA_ADMIN_USERNAME = 'admin'
+GRAFANA_ADMIN_PASSWORD = 'admin'
+GRAFANA_URL = 'grafana.me.com'
+
+# md5 Salts
+MD5_KEY_PASSWORD = 'thisIsASalt'
+MD5_KEY_LOGINTOKEN = 'thisIsASalt'
